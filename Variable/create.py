f4ae6b5af6d5998a7ae9e9c6b98d18099a0faf9d
codewithbLanksS <apiwat.pu@ku.th>
2021-01-10 14:16:25 +0700

    add create gettypes , single or double variable

---
+++ Variable/create.py
@@ -0,0 +1,14 @@
+name = "Apiwat Puangrod"
+age = 23
+weight = 69.5
+
+# boolean | can is has
+
+can_walk = True
+can_run = False
+
+print(name)
+print(age)
+print(weight)
+print(can_walk)
+print(can_run)
---
+++ Variable/single_or_double.py
@@ -0,0 +1,2 @@
+name = "bank"
+name = "bank"
